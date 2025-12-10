0cf06d70eb03a8f07807b6b9de53fdc5345dc7cb
miguelbaumann1981 <miguelmartinezm@outlook.es>
2025-12-10 09:52:27 +0100

    output messages

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-console.log('Hola mundo 2');
+
+const fs = require('fs')
+
+// async function createDocument() {
+//     let fileHandle;
+//     try {
+//         fileHandle = await fs.open('myfile.txt', 'w');
+//         await fileHandle.write(header);
+//         for (let i = 1; i < 10; i++) {
+//             const operation =  5 * i;
+//             const result = (`5 x ${i} = ${operation}\n`).toString()
+//             await fileHandle.write(result);
+//         }
+//     } catch(error) {
+//         console.error('Error writing files:', error);
+//     }
+// }
+//  createDocument();
+
+
+const header: string = '=============\nTabla del 5\n=============\n';
+let outputMessage: string = '';
+for (let i = 1; i < 10; i++) {
+    const operation =  5 * i;
+    outputMessage += `5 x ${i} = ${operation}\n`
+}
+outputMessage = header + outputMessage;
+
+const outputPath = `outputs`;
+fs.mkdirSync(outputPath, {recursive: true});
+
+fs.writeFileSync(`${outputPath}/myfile.txt`, outputMessage);
+
+
